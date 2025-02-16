3f48122dccdd3bf52ab1d8444ec624ecf7e4b2c4
PM <git@pwm1991.com>
2025-02-16 13:43:12 +0000

    Add set parser unit tests

---
+++ api/hevy/src/parseSets/index.test.js
@@ -0,0 +1,66 @@
+const { parseSets, POUNDS_TO_KG } = require('./index');
+const pino = require('pino');
+
+const log = pino({
+    level: process.env.PINO_LOG_LEVEL || 'debug',
+    timestamp: pino.stdTimeFunctions.isoTime,
+    redact: {
+        paths: ['email', 'HEVY_KEY'],
+    },
+});
+
+const exampleSetInKg = [
+    {
+        index: 0,
+        type: 'normal',
+        weight_kg: 15,
+        reps: 15,
+        distance_meters: null,
+        duration_seconds: null,
+        rpe: null,
+    },
+    {
+        index: 0,
+        type: 'normal',
+        weight_kg: 60,
+        reps: 8,
+        distance_meters: null,
+        duration_seconds: null,
+        rpe: null,
+    },
+];
+
+const exampleSetInLbs = [
+    {
+        index: 0,
+        type: 'normal',
+        weight_lb: 15,
+        reps: 15,
+        distance_meters: null,
+        duration_seconds: null,
+        rpe: null,
+    },
+];
+
+describe('parseSets', () => {
+    describe('when sets are in kg', () => {
+        test('should return a parsed set object', () => {
+            const expectedOutcome = [
+                { reps: 15, weight: 15, totalWeight: 225 },
+                { reps: 8, weight: 60, totalWeight: 480 },
+            ];
+            const result = parseSets(exampleSetInKg).sets;
+            expect(result).toEqual(expectedOutcome);
+        });
+        test('should sum the totalWeight in KG correctly', () => {
+            const result = parseSets(exampleSetInKg);
+            expect(result.setsTotalWeight).toBe(705);
+        });
+    });
+    describe('when sets are in lbs', () => {
+        test('should sum the totalWeight in LBs correctly', () => {
+            const result = parseSets(exampleSetInLbs);
+            expect(result.setsTotalWeight).toBe(225 * POUNDS_TO_KG);
+        });
+    });
+});
